61e58c983d5ff8594f8221e0f088dcfe23763ccf
Luciano <lucianocaffarone@gmail.com>
2023-04-08 13:18:27 +0200

    Desktop-Finish

---
+++ HTML/main.js
@@ -0,0 +1,20 @@
+
+
+
+ScrollReveal().reveal('.about' , {delay: 200});
+
+ScrollReveal().reveal('.grid' , {delay: 200});
+
+
+ScrollReveal().reveal('.lenguajes' , {delay: 200});
+
+ScrollReveal().reveal('.proyects' , {delay: 200});
+
+
+ScrollReveal().reveal('.grid-contenido' , {delay: 200});
+
+
+
+
+
+
