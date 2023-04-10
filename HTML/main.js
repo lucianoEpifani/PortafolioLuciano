585e086b9aa46155b24f8cf5e076a3de2ab8abde
Luciano <lucianocaffarone@gmail.com>
2023-04-10 09:46:06 +0200

    Mq-modify

--- HTML/main.js
+++ HTML/main.js
@@ -14,17 +14,6 @@ cerrar.addEventListener('click',() =>{
 
 
 
-ScrollReveal().reveal('.about' , {delay: 200});
-
-ScrollReveal().reveal('.grid' , {delay: 200});
-
-
-ScrollReveal().reveal('.lenguajes' , {delay: 200});
-
-ScrollReveal().reveal('.proyects' , {delay: 200});
-
-
-ScrollReveal().reveal('.grid-contenido' , {delay: 200});
 
 
 
